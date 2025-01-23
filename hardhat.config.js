680a4fdab8f1da2ec94cd51d03d3c38faa2ed3ff
Aleeza Hussain <aleezahussain237@gmail.com>
2025-01-23 21:25:34 +0500

    initial setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,10 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  paths: {
+    sources: "./blockchain/contracts", // Path to your contracts folder
+    tests: "./blockchain/test",       // Path to your test folder
+    scripts: "./blockchain/scripts",  // Path to your scripts folder
+},
+
 };
