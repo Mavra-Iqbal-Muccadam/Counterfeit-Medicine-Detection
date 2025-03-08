a1019daa698d0e7c597c5f76f80ddf16da732f33
Aleeza Hussain <aleezahussain237@gmail.com>
2025-03-08 15:09:19 +0500

    pushing before pulling khatija

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,4 +1,4 @@
-import { supabase } from '../../../lib/supabaseClient';
+import { supabase } from '../../../lib/supabaseClientanon';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { serialize } from 'cookie';
--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,4 +1,4 @@
-import { supabase } from '../../../lib/supabaseClient'; 
+import { supabase } from '../../../lib/supabaseClientanon';
 import bcrypt from 'bcryptjs';
 
 export default async function handler(req, res) {
