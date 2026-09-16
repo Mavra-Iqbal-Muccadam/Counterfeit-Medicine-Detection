9a46a5e043665c23e697d59c6c92f305e1089061
Mavra Iqbal Muccadam <mavra.iqbal@muccadam.com>
2026-09-16 22:22:52 +0500

    fix: merge next.config.js exports and ignore ESLint during builds

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
-require("dotenv").config(); 
+require("dotenv").config();
 
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   env: {
     BLOCKCHAIN_RPC_URL: process.env.BLOCKCHAIN_RPC_URL,
     PRIVATE_KEY: process.env.PRIVATE_KEY,
     CONTRACT_ADDRESS: process.env.CONTRACT_ADDRESS,
   },
-};
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
   compiler: {
     styledComponents: true,
   },
